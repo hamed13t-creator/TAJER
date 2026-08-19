f94027412035b8185ed7f3924e9f183f8b0f6b34
hamed13t-creator <hamed13t@gmail.com>
2026-08-19 21:51:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'tajer-v10';
+const CACHE = 'tajer-v11';
 const ASSETS = ['./index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
